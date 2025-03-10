84dc11f0fcf83546b7d5230363ef703ca4ee5171
Davis Liu <davisliu2006@gmail.com>
2025-03-10 00:44:37 -0400

    Update judge.ts

--- judge/judge.ts
+++ judge/judge.ts
@@ -6,24 +6,32 @@ let docker = new Docker();
 // execute code
 export async function execute(language: string, code: string) {
     try {
+        console.log("Judging "+language+"...");
         // execute code in docker container
-        let [container, output]: [Docker.Container, Buffer] = await docker.run(
+        let [status, container]: [object, Docker.Container] = await docker.run(
             "online_judge", // docker image name
-            [language, code],     // command to run (language)
+            [language, code], // command to run (language)
             process.stdout, // stream stdout to the host
             {
                 HostConfig: {
                     Memory: 256*1024*1024, // memory limit (256 MB)
-                    CpuPeriod: 100000,     // cpu period
-                    CpuQuota: 100000,      // cpu quota (1 second)
-                    NetworkMode: "none",   // disable network access
-                    AutoRemove: true,      // remove container after execution
+                    CpuPeriod: 100000, // cpu period
+                    CpuQuota: 100000, // cpu quota (1 second)
+                    NetworkMode: "none", // disable network access
+                    AutoRemove: true, // remove container after execution
                 },
                 WorkingDir: "/home/judge", // working directory in the container
             }
         );
 
-        console.log("Output:", output.toString());
+        console.log(status);
+        const logsStream = await container.logs({
+            follow: false, // don't follow real time output
+            stdout: true, // stdout
+            stderr: false // stderr
+        });
+        let output: string = logsStream.toString(); // convert logs to string
+        console.log('Output:', output);
     } catch (e) {
         console.log("Judge failed");
         console.error(e);
