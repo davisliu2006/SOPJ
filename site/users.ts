4d065e7beddeb39d5b5f7ff7e649fcd89e70cdbb
Davis Liu <davisliu2006@gmail.com>
2025-03-13 22:20:49 -0400

    Update frontend

--- site/users.ts
+++ site/users.ts
@@ -4,14 +4,21 @@ import * as globals from "./globals";
 export async function users(req: express.Request, res: express.Response) {
     try {
         let user = req.user;
+        let search = (typeof(req.query["search"]) == "string"? req.query["search"] : "");
         let users: Array<any> = [];
         let conn = await globals.pool.getConnection();
         globals.dbSetup.initUsers(conn);
-        let rows = await conn.query("SELECT id, username, points FROM users;");
+        let rows;
+        if (search == "") {
+            rows = await conn.query("SELECT id, username, points FROM users;");
+        } else {
+            rows = await conn.query(`SELECT id, username, points FROM users WHERE username LIKE '%${search}%';`);
+        }
         conn.release();
         users = rows;
         res.render("users.ejs", {user, users});
-    } catch {
+    } catch (e) {
+        console.log(e);
         res.redirect("/error-500");
     }
 }
