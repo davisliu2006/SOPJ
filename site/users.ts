f0939d47a95d74ce2f7d82e5e8d36734f8ab7e9c
Davis Liu <davisliu2006@gmail.com>
2025-05-03 00:24:03 -0400

    Add solved problems to user page

--- site/users.ts
+++ site/users.ts
@@ -33,10 +33,33 @@ export async function users_view(req: express.Request, res: express.Response) {
         let userID = (req.query["id"]? req.query["id"] : "");
         let conn = await globals.pool.getConnection();
         let rows = await conn.query("SELECT id, username, points FROM users WHERE id = ?;", [userID]);
-        conn.release();
         let userV = rows[0];
-        res.render("users-view.ejs", {user, userV});
-    } catch {
+        let submissions = await conn.query(
+            "SELECT id, problem, status, points, totpoints FROM submissions WHERE user = ? AND points != 0 ORDER BY points;",
+            [userID]
+        );
+        let solvedP: Array<any> = [];
+        let st = new Set<number>();
+        for (let submission of submissions) {
+            let problem = (await conn.query(
+                "SELECT id, name, points FROM problems WHERE id = ?;",
+                [submission.problem])
+            )[0];
+            if (st.has(problem.id)) {continue;}
+            st.add(problem.id);
+            problem.ptsEarned = problem.points * submission.points/submission.totpoints;
+            problem.verdict = submission.status;
+            solvedP.push(problem);
+        }
+        solvedP.sort(function(x, y) {
+            if (x.ptsEarned > y.ptsEarned) {return -1;}
+            else if (x.ptsEarned < y.ptsEarned) {return 1;}
+            else {return 0;}
+        });
+        conn.release();
+        res.render("users-view.ejs", {user, userV, solvedP});
+    } catch (e) {
+        console.log(e);
         res.redirect("/error-500");
     }
 }
