9fa3e397a93af4bd41bd8d9036a2ebf251b19cf8
Davis Liu <davisliu2006@gmail.com>
2025-05-06 13:17:46 -0400

    Add popular problems view

--- site/index.ts
+++ site/index.ts
@@ -1,8 +1,38 @@
 import express from "express";
+import * as globals from "./globals";
 
-export function index(req: express.Request, res: express.Response) {
+export async function index(req: express.Request, res: express.Response) {
     let user = req.user;
-    res.render("index.ejs", {user});
+    let conn = await globals.pool.getConnection();
+
+    // popular problems
+    let submissions = await conn.query(`SELECT id, problem FROM submissions ORDER BY id DESC LIMIT 100;`);
+    let freq = new Map<number,number>();
+    for (let submission of submissions) {
+        if (!freq.has(submission.problem)) {
+            freq.set(submission.problem, 1);
+        } else {
+            freq.set(submission.problem, freq.get(submission.problem)+1);
+        }
+    }
+    let sorted = [];
+    for (let key of freq.keys()) {
+        sorted.push(key);
+    }
+    sorted.sort(function(x, y) {
+        if (freq.get(x) > freq.get(y)) {return -1;}
+        else if (freq.get(x) < freq.get(y)) {return 1;}
+        else {return 0;}
+    });
+    let popular = [];
+    for (let id of sorted) {
+        popular.push(
+            (await conn.query("SELECT id, name FROM problems WHERE id = ?;", [id]))[0]
+        );
+    }
+
+    conn.release();
+    res.render("index.ejs", {user, popular});
 }
 
 export function about(req: express.Request, res: express.Response) {
